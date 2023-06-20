462a706a0b6c881d04fedab18d0e0b943d83304a
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-06-21 00:44:38 +0700

    feat:  Environment setup, navbar UI, Auth UI, Register functionality-mongodb-prisma set up, login functionality.

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import ClientOnly from './components/ClientOnly'
+// import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import Navbar from './components/navbar/Navbar'
+import './globals.css'
+import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"]
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='font.className'>
+        <ClientOnly>
+
+          <ToasterProvider />
+
+          <LoginModal />
+
+          <RegisterModal />
+
+          {/* <Modal actionLabel='Submit' title='Hello World' isOpen /> */}
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  )
+}
