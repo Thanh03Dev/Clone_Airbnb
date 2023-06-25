49a6b11e1305f91333f99a547136cf0a607577fd
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-06-25 17:22:26 +0700

    feat: categories UI, listing creation step 1 ( category seclection )

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 // import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
 import { StafeUser } from "@/app/types";
+import useRentModal from "@/app/hooks/useRentModal";
 
 interface UserMenuProps {
   currentUser?: StafeUser | null;
@@ -25,12 +26,21 @@ const UserMenu: React.FC<UserMenuProps> = ({
 
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal();
+
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+  }, [currentUser, loginModal, rentModal])
 
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          // onClick={()=>{}}
+          onClick={onRent}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           Airbnb your home
@@ -55,7 +65,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 <MenuItem onClick={() => { }} label="My favorites" />
                 <MenuItem onClick={() => { }} label="My reservations" />
                 <MenuItem onClick={() => { }} label="My properties" />
-                <MenuItem onClick={() => { }} label="Airbnb my home" />
+                <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
               </>
