a02d3bc66a210c45447eefcda1b68ed78a1b3a1f
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-07-11 02:37:38 +0700

    test GetServerSideProps

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps, NextPage } from 'next';
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -5,18 +6,12 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-
 interface HomeProps {
-  searchParams: IListingsParams
+  listings: any[]; // Thay any[] bằng kiểu dữ liệu thích hợp cho listings
+  currentUser: any; // Thay any bằng kiểu dữ liệu thích hợp cho currentUser
 }
 
-const Home = async ({ searchParams }: HomeProps) => {
-
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-
-  // const isEmpty = true;
-
+const Home: NextPage<HomeProps> = ({ listings, currentUser }) => {
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -29,19 +24,33 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              < ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            )
-          })}
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   )
 }
 
-export default Home
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const searchParams: IListingsParams = {
+    // Các thông số tìm kiếm
+  };
+
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  return {
+    props: {
+      listings,
+      currentUser,
+    },
+  };
+};
+
+export default Home;
