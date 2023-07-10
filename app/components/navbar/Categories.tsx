cf0b59e65db78561a171038cbd81872425999f60
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-07-10 13:36:39 +0700

    fix icons

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,90 +1,92 @@
+'use client';
+
 import Container from "../Container";
-import { SvgIcon } from "@mui/material";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb"
 import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from "react-icons/gi"
-import { MdOutlineVilla } from "@mui/icons-material";
-import { FaSkiing } from "@mui/icons-material";
-import { BsSnow } from "@mui/icons-material";
-import { IoDiamond } from "@mui/icons-material";
+import { MdOutlineVilla } from "react-icons/md"
+import { FaSkiing } from "react-icons/fa"
+import { BsSnow } from "react-icons/bs"
+import { IoDiamond } from "react-icons/io5"
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
     {
         label: "Beach",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is close to the beach!",
+        icon: TbBeach,
+        description: "This property is close to the beach!"
     },
     {
         label: "Windmills",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property has windmills!",
+        icon: GiWindmill,
+        description: "This property has windmills!"
     },
     {
         label: "Modern",
-        icon: () => <MdOutlineVilla />,
-        description: "This property is modern!",
+        icon: MdOutlineVilla,
+        description: "This property is modern!"
     },
     {
         label: "Countryside",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is in the countryside!",
+        icon: TbMountain,
+        description: "This property is in the countryside!"
     },
     {
         label: "Pools",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property has a pool!",
+        icon: TbPool,
+        description: "This property has a pool!"
     },
     {
         label: "Islands",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is on an island!",
+        icon: GiIsland,
+        description: "This property is on an island!"
     },
     {
         label: "Lake",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is close to a Lake!",
+        icon: GiBoatFishing,
+        description: "This property is close to a Lake!"
     },
     {
         label: "Skiing",
-        icon: () => <FaSkiing />,
-        description: "This property has skiing activities!",
+        icon: FaSkiing,
+        description: "This property has skiing activities!"
     },
     {
         label: "Castles",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is in a casles!",
+        icon: GiCastle,
+        description: "This property is in a casles!"
     },
     {
         label: "Camping",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property has camping activities!",
+        icon: GiForestCamp,
+        description: "This property has camping activities!"
     },
     {
         label: "Arctic",
-        icon: () => <BsSnow />,
-        description: "This property has camping activities!!",
+        icon: BsSnow,
+        description: "This property has camping activities!!"
     },
     {
         label: "Cave",
-        icon: () => <SvgIcon component={GiWindmill} />,
-        description: "This property is in a cave!",
+        icon: GiCaveEntrance,
+        description: "This property is in a cave!"
     },
     {
         label: "Desert",
-        icon: () => <GiCactus />,
-        description: "This property is in the desert!",
+        icon: GiCactus,
+        description: "This property is in the desert!"
     },
     {
         label: "Barns",
-        icon: () => <GiBarn />,
-        description: "This property is in the barn!",
+        icon: GiBarn,
+        description: "This property is in the barn!"
     },
     {
         label: "Lux",
-        icon: () => <IoDiamond />,
-        description: "This property is luxurious!",
+        icon: IoDiamond,
+        description: "This property is luxurious!"
     },
-];
+]
 
 const Categories = () => {
     const params = useSearchParams();
@@ -93,7 +95,7 @@ const Categories = () => {
 
     const pathname = usePathname();
 
-    const isMainPage = pathname === "/";
+    const isMainPage = pathname === '/';
 
     if (!isMainPage) {
         return null;
@@ -107,12 +109,12 @@ const Categories = () => {
                         key={item.label}
                         label={item.label}
                         selected={category === item.label}
-                        icon={item.icon()}
+                        icon={item.icon}
                     />
                 ))}
             </div>
         </Container>
-    );
-};
+    )
+}
 
-export default Categories;
+export default Categories
